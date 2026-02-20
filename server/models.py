6e14c86bcc03d44f561eac0fd682ed919e345a52
Joshua Clay <josh.t.clay1995@gmail.com>
2026-02-19 21:19:40 -0700

    feat: define Exercise, Workout, and WorkoutExercise models with relationships

--- server/models.py
+++ server/models.py
@@ -2,4 +2,49 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
 db = SQLAlchemy()
 
-# Define Models here
+class Exercise(db.Model):
+    __tablename__ = 'exercises'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    category = db.Column(db.String)
+    equipment_needed = db.Column(db.Boolean)
+
+    # Relationship to the join table
+    workout_exercises = db.relationship('WorkoutExercise', back_populates='exercise', cascade='all, delete-orphan')
+
+    def __repr__(self):
+        return f'<Exercise {self.name}>'
+
+class Workout(db.Model):
+    __tablename__ = 'workouts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date) 
+    duration_minutes = db.Column(db.Integer)
+    notes = db.Column(db.String) 
+
+    # Relationship to the join table
+    workout_exercises = db.relationship('WorkoutExercise', back_populates='workout', cascade='all, delete-orphan')
+
+    def __repr__(self):
+        return f'<Workout {self.id} on {self.date}>'
+
+class WorkoutExercise(db.Model):
+    __tablename__ = 'workout_exercises'
+
+    id = db.Column(db.Integer, primary_key=True)
+    workout_id = db.Column(db.Integer, db.ForeignKey('workouts.id'))
+    exercise_id = db.Column(db.Integer, db.ForeignKey('exercises.id'))
+    
+    # The extra data on the join table
+    reps = db.Column(db.Integer)
+    sets = db.Column(db.Integer)
+    duration_seconds = db.Column(db.Integer)
+
+    # Relationships mapping back to the parent tables
+    workout = db.relationship('Workout', back_populates='workout_exercises')
+    exercise = db.relationship('Exercise', back_populates='workout_exercises')
+
+    def __repr__(self):
+        return f'<WorkoutExercise Workout:{self.workout_id} Exercise:{self.exercise_id}>'
